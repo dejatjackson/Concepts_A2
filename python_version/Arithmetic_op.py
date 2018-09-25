5d6b013a1a9d6f5477a65d03c43a9307d3b27179
Deja Jackson <dejatjackson@gmail.com>
2018-09-25 11:47:36 -0400

    Adding new Classes

---
+++ python_version/Arithmetic_op.py
@@ -0,0 +1,4 @@
+from enum import Enum
+
+class Arithmetic_op(Enum):
+	add_operator, sub_operator, mul_operator, mod_operator, rev_div_operator, exp_operator
---
+++ python_version/Block.py
@@ -0,0 +1,11 @@
+
+class Block:
+
+	//TODO
+	def _init_():
+
+	def add():
+
+	def process():
+
+	def size():
