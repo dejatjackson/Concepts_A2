3be28217bd408a7a1116d575c249e9c81e6c0793
Deja Jackson <dejatjackson@gmail.com>
2018-10-02 11:15:16 -0400

    Fixing Enums

--- python_version/Relative_op.py
+++ python_version/Relative_op.py
@@ -1,5 +1,9 @@
 from enum import Enum
 
 class Relative_op(Enum):
-
-	le_operator, lt_operator, ge_operator, gt_operator, eq_operator, ne_operator
+	le_operator = "<="
+	lt_operator = "<"
+	ge_operator = ">="
+	gt_operator = ">"
+	ne_operator = "!="
+	eq_operator = "=="
--- python_version/tokentype.py
+++ python_version/tokentype.py
@@ -1,13 +1,24 @@
 import enum
 
+
 class tokentype(enum.Enum):
-
-
-	#function_tok,end_tok, if_tok, else_tok, print_tok, for_tok, while_tok, id, literal_integer, assignment_operator, le_operator, lt_operator, ge_operator,
-    #gt_operator, eq_operator, ne_operator, exp_operator, EOS_TOK
+    # TODO id, literal_integer, EOS_TOK
+    function_tok = "function"
+    end_tok = "end"
+    if_tok = "if"
+    else_tok = "else"
+    print_tok = "print"
+    for_tok = "for"
+    while_tok = "while"
+    le_operator = "<="
+    lt_operator = "<"
+    ge_operator = ">="
     gt_operator = ">"
+    ne_operator = "!="
+    eq_operator = "=="
+    assignment_operator = "="
     add_operator = "+"
-	sub_operator = "-"
-	mul_operator = "*"
-	mod_operator = "%"
-	exp_operator = "^"
+    sub_operator = "-"
+    mul_operator = "*"
+    mod_operator = "%"
+    exp_operator = "^"
