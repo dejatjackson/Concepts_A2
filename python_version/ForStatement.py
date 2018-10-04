5d6fc01a3aae018169473ef1bd6debd9af3a1295
Ezekiel Lawal <Zeke@Ezekiels-MacBook-Pro.local>
2018-10-04 10:55:31 -0400

    Added ForStatement

---
+++ python_version/ForStatement.py
@@ -0,0 +1,21 @@
+class ForStatement:
+
+    def __init__( self, bool, blk):
+
+        if bool is None:
+            raise ValueError("null boolean expression")
+        elif blk is None:
+            raise ValueError("null black")
+
+        self.bool = bool;
+        self.blk = blk;
+
+
+    def execute(self):
+        #while(bool.eval()):
+            #blk.process()
+
+        while True:
+            self.blk.process()
+            if self.bool.eval() != True:
+                break
